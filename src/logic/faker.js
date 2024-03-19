28391a56273a11f062d0b7aae748b0b0fe1d1232
mdg0410 <levoyersteven@gmail.com>
2024-03-18 22:55:33 -0500

    Create shopping cards

---
+++ src/logic/faker.js
@@ -0,0 +1,13 @@
+import { faker } from '@faker-js/faker';
+
+const generateProduct = () => {
+  return {
+    id: faker.string.uuid(),
+    image: faker.image.url(),
+    description: faker.commerce.productDescription(),
+  };
+};
+
+const products = Array.from({ length: 10 }, generateProduct);
+
+export default products;
